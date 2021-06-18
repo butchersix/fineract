306fd2d47f5bc4c787821d49785110bfd8450c0f
butchersix <jasperhavenbriones@gmail.com>
2021-06-18 22:21:16 +0800

    [UPDATE] Functional TenantApiResource for db migration api call

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/tenant/api/TenantsApiResource.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/tenant/api/TenantsApiResource.java
@@ -18,28 +18,35 @@
  */
 package org.apache.fineract.portfolio.tenant.api;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.sql.DataSource;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.apache.fineract.infrastructure.core.serialization.DefaultToApiJsonSerializer;
+import org.apache.fineract.infrastructure.core.service.TenantDatabaseUpgradeService;
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
+import org.apache.fineract.infrastructure.security.service.TenantDetailsService;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Path("/tenants")
 @Component
@@ -48,45 +55,66 @@ public class TenantsApiResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(TenantsApiResource.class);
 
-    private final PlatformSecurityContext context;
+    private final PlatformSecurityContext platformSecurityContext;
     private final DefaultToApiJsonSerializer toApiJsonSerializer;
 
+    private final TenantDetailsService tenantDetailsService;
+
     @Autowired
-    public TenantsApiResource(final PlatformSecurityContext context,
-            final DefaultToApiJsonSerializer<TenantsApiResource> toApiJsonSerializer) {
-        this.context = context;
+    private TenantDatabaseUpgradeService tenantDatabaseUpgradeService;
+
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    public TenantsApiResource(final PlatformSecurityContext platformSecurityContext,
+            final DefaultToApiJsonSerializer<TenantsApiResource> toApiJsonSerializer, final TenantDetailsService detailsService) {
+        this.platformSecurityContext = platformSecurityContext;
         this.toApiJsonSerializer = toApiJsonSerializer;
+        this.tenantDetailsService = detailsService;
     }
 
     @POST
     @Path("create")
     @Consumes({ MediaType.APPLICATION_JSON })
     @Produces({ MediaType.APPLICATION_JSON })
-    public ResponseEntity createDatabase(@RequestBody Map<String, String> params) {
+    @SuppressWarnings("try")
+    public ResponseEntity createDatabase(@Parameter(hidden = true) final String apiRequestBodyAsJson) {
 
-        if (isIncomplete(Arrays.asList("schema_name"))) {
+        Map<String, Object> params = new Gson().fromJson(apiRequestBodyAsJson, new TypeToken<HashMap<String, Object>>() {}.getType());
+
+        if (isIncomplete(Arrays.asList("identifier", "username", "password", "params"))) {
             // TODO
             LOG.error("TenantsApiResource:createDatabase - Invalid schema_name");
             return new ResponseEntity<Object>("Invalid schema_name", HttpStatus.NOT_FOUND);
         } else {
             try {
                 // TODO: create tenant database
+                String identifier = params.get("identifier").toString();
+                String defaultParams = params.get("params").toString();
+                String username = params.get("username").toString();
+                String password = params.get("password").toString();
+                String dbUrl = getEnvVar("PROTOCOL", "jdbc") + ":mysql://localhost:3306/fineract_" + identifier + defaultParams;
+                String dbUid = username;
+                String dbPwd = password;
 
-                String dbUrl = "jdbc:mysql://127.0.0.1:3306";
-                String dbUid = getEnvVar("FINERACT_DEFAULT_TENANTDB_UID", "root");
-                String dbPwd = getEnvVar("FINERACT_DEFAULT_TENANTDB_PWD", "mysql");
+                // try {
+                // FineractPlatformTenant tenant = tenantDetailsService.loadTenantById(identifier);
+                // return new ResponseEntity<Object>("Tenant already exists.", HttpStatus.CONFLICT);
+                // } catch (InvalidTenantIdentiferException e) {
+                // }
 
                 LOG.info("url: {}, username: {}, password: {}", dbUrl, dbUid, dbPwd);
 
-                // SQL command to create a database in MySQL.
-                String sql = "CREATE DATABASE IF NOT EXISTS " + params.get("schema_name");
+                DataSource dataSource = DataSourceBuilder.create().driverClassName("com.mysql.cj.jdbc.Driver").url(dbUrl).username(dbUid)
+                        .password(dbPwd).build();
 
-                Connection conn = DriverManager.getConnection(dbUrl, dbUid, dbPwd);
-                PreparedStatement stmt = conn.prepareStatement(sql);
+                try (Connection c = dataSource.getConnection()) {
+                    tenantDatabaseUpgradeService.upgradeAllTenants();
+                    LOG.info("Tenant '{}' added.", identifier);
+                }
 
-                stmt.execute();
-
-                return new ResponseEntity<Object>("Tenant database successfully created", HttpStatus.OK);
+                return new ResponseEntity<Object>(String.format("Tenant `%s` database successfully created", identifier), HttpStatus.OK);
 
             } catch (Exception e) {
                 LOG.error("TenantsApiResource:createDatabase - Exception: {}", e.getMessage());
@@ -113,8 +141,9 @@ public class TenantsApiResource {
     }
 
     private String getEnvVar(String name, String defaultValue) {
-        String value = System.getenv(name);
-        if (value == null) {
+        Environment environment = context.getEnvironment();
+        String value = environment.getProperty(name);
+        if (value == null || value.isEmpty()) {
             return defaultValue;
         }
         return value;
